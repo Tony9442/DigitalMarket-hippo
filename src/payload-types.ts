4e5187129d89ac1fab27970eeff4069a40395be3
Tony9442 <112698587+Tony9442@users.noreply.github.com>
2024-04-13 03:14:50 +0800

    new-change

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -24,6 +24,8 @@ export interface Config {
  */
 export interface User {
   id: string;
+  products?: (string | Product)[] | null;
+  product_files?: (string | ProductFile)[] | null;
   role: 'admin' | 'user';
   updatedAt: string;
   createdAt: string;
@@ -37,6 +39,44 @@ export interface User {
   loginAttempts?: number | null;
   lockUntil?: string | null;
   password: string | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "products".
+ */
+export interface Product {
+  id: string;
+  user?: (string | null) | User;
+  name: string;
+  description?: string | null;
+  price: number;
+  category: 'ui_kits' | 'icons';
+  product_files: string | ProductFile;
+  approvedForSale?: ('pending' | 'approved' | 'denied') | null;
+  priceId?: string | null;
+  stripeId?: string | null;
+  images: {
+    image: string | Media;
+    id?: string | null;
+  }[];
+  updatedAt: string;
+  createdAt: string;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "product_files".
+ */
+export interface ProductFile {
+  id: string;
+  user?: (string | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  url?: string | null;
+  filename?: string | null;
+  mimeType?: string | null;
+  filesize?: number | null;
+  width?: number | null;
+  height?: number | null;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -79,44 +119,6 @@ export interface Media {
       filename?: string | null;
     };
   };
-}
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "products".
- */
-export interface Product {
-  id: string;
-  user?: (string | null) | User;
-  name: string;
-  description?: string | null;
-  price: number;
-  category: 'ui_kits' | 'icons';
-  product_files: string | ProductFile;
-  approvedForSale?: ('pending' | 'approved' | 'denied') | null;
-  priceId?: string | null;
-  stripeId?: string | null;
-  images: {
-    image: string | Media;
-    id?: string | null;
-  }[];
-  updatedAt: string;
-  createdAt: string;
-}
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "product_files".
- */
-export interface ProductFile {
-  id: string;
-  user?: (string | null) | User;
-  updatedAt: string;
-  createdAt: string;
-  url?: string | null;
-  filename?: string | null;
-  mimeType?: string | null;
-  filesize?: number | null;
-  width?: number | null;
-  height?: number | null;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import { IncomingMessage } from 'http';
 import { stripeWebhookHandler } from './webhooks';
 import nextBuild from "next/dist/build"
 import path from "path";
+import { PayloadRequest } from "payload/types";
+import { parse } from "url";
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
@@ -59,9 +61,21 @@ const start = async () => {
 
     return
   }
-   
+      const cartRouter = express.Router();
 
-   app.use(
+      cartRouter.use(payload.authenticate);
+
+      cartRouter.get("/", (req, res) => {
+        const request = req as PayloadRequest;
+
+        if (!request.user) return res.redirect("/sign-in?origin=cart");
+
+        const parsedUrl = parse(req.url, true);
+        const { query } = parsedUrl;
+
+        return nextApp.render(req, res, "/cart", query);
+      });
+  app.use(
     '/api/trpc', 
     trpcExpress.createExpressMiddleware({
     router: appRouter,
